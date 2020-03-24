28f9e584f0c256c0d03318e4b8a7d56d713d26e7
SohyunNam <56374318+SohyunNam@users.noreply.github.com>
2020-03-24 21:25:50 +0900

    통합 SimComponents에 맞춰 수정

--- supply_chain_gen.py
+++ supply_chain_gen.py
@@ -6,14 +6,15 @@ import os
 import pandas as pd
 from collections import OrderedDict
 import matplotlib.pyplot as plt
-from SimComponents_master_plan import DataframeSource, Sink, Process, Monitor
+from SimComponents import Source, Sink, Process, Monitor
 
+start_0 = time.time()
 data_all = pd.read_excel('./data/spool_data_for_simulation.xlsx')
 data = data_all[['NO_SPOOL', '제작협력사', '도장협력사', "Plan_makingLT", "Actual_makingLT", "Predicted_makingLT",
                  "Plan_paintingLT", "Actual_paintingLT", "Predicted_paintingLT"]]
 
 df = pd.DataFrame()
-df['part_no'] = data['NO_SPOOL']
+# df['part_no'] = data['NO_SPOOL']
 df['process1'] = data['제작협력사'] + '_1'
 df['process2'] = data['도장협력사'] + '_2'
 
@@ -21,18 +22,28 @@ df['process2'] = data['도장협력사'] + '_2'
 df['proc_time_1'] = data['Actual_makingLT']
 df['proc_time_2'] = data['Actual_paintingLT']
 
+## IAT
 adist = functools.partial(random.randrange, 1, 10)
 
 def generator(block_data):
+    start_time = 0
     for i in range(len(block_data)):
+        srs = pd.Series()
         temp_block_data = block_data.iloc[i]
-        part_name = temp_block_data['part_no']
 
-        dict_activity = OrderedDict()
-        dict_activity[temp_block_data['process1']] = [adist(), temp_block_data['proc_time_1']]
-        dict_activity[temp_block_data['process2']] = [0, temp_block_data['proc_time_2']]
+        start_time += adist()
+        temp_series_1 = pd.Series([start_time, temp_block_data['proc_time_1'], temp_block_data['process1']],
+                                  index=[[0, 0, 0], ['start_time', 'process_time', 'process']])
+        srs = pd.concat([srs, temp_series_1])
 
-        yield [part_name, dict_activity]
+        temp_series_2 = pd.Series([start_time, temp_block_data['proc_time_2'], temp_block_data['process2']],
+                                  index=[[1, 1, 1], ['start_time', 'process_time', 'process']])
+        srs = pd.concat([srs, temp_series_2])
+
+        temp_series_3 = pd.Series([None, None, 'Sink'], index=[[2, 2, 2], ['start_time', 'process_time', 'process']])
+        srs = pd.concat([srs, temp_series_3])
+
+        yield srs
 
 
 gen_block_data = generator(df)
@@ -42,24 +53,18 @@ RUN_TIME = 45000
 env = simpy.Environment()
 
 process_dict = {}
-Source = DataframeSource(env, 'Source', gen_block_data, process_dict, len(df))
+Source = Source(env, 'Source', gen_block_data, process_dict, len(df), data_type="gen")
 Sink = Sink(env, 'Sink', rec_lead_time=True, rec_arrivals=True)
 
 process_list = list(df.drop_duplicates(['process1'])['process1'])
 process_list += list(df.drop_duplicates(['process2'])['process2'])
 
 process = []
-monitor = []
-monitor_dict = {}
 for i in range(len(process_list)):
     process.append(Process(env, process_list[i], 1, process_dict, 10000))
 
-'''for i in range(len(process_list)):
-    monitor.append(Monitor(env, process[i], 10))'''
-
 for i in range(len(process_list)):
     process_dict[process_list[i]] = process[i]
-    # monitor_dict[process_list[i]] = monitor[i]
 
 process_dict['Sink'] = Sink
 
@@ -68,33 +73,19 @@ start = time.time()
 env.run()
 finish = time.time()
 
-print(finish - start)
-# print(Sink.block_project_sim)
+print("total time ", finish - start_0)
+print("simulation time : ", finish - start)
 
 
-def utilization(activity, m):
+def utilization(activity):
     temp_process = process_dict[activity]
-    total_time = (temp_process.process_finish - temp_process.process_start) * m
-    total_working = dict_activity[activity]['total working time']
+    total_time = (temp_process.process_finish - temp_process.process_start) * temp_process.server_num
+    total_working = temp_process.working_time
     u = total_working / total_time
 
     return u
 
 
-dict_activity = {}
 for process in process_list:
-    temp = df[(df['process1'] == process) | (df['process2'] == process)]
-    if process[-1] == 1:
-        sum = temp['proc_time_1'].sum()
-    else:
-        sum = temp['proc_time_2'].sum()
-    dict_activity[process] = {}
-    dict_activity[process]['total working time'] = sum
-
-    dict_activity[process]['u'] = utilization(process, 1)
-
-    dict_activity[process]['m'] = 1
-
-for process in process_list:
-    print('utilization of {} : '.format(process), dict_activity[process]['u'])
+    print('utilization of {} : '.format(process), utilization(process))
 
