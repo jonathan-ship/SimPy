f5b9093f8b6960c3c214d4c549c755df4488df7d
SohyunNam <56374318+SohyunNam@users.noreply.github.com>
2020-04-23 14:39:17 +0900

    변경된 Event Tracer에 맞춰 일부 수정 및 주석 추가

--- supply_chain_df.py
+++ supply_chain_df.py
@@ -1,29 +1,35 @@
+import os
 import simpy
 import time
 import pandas as pd
 import scipy.stats as st
-import matplotlib.pyplot as plt
-from SimComponents import Source, Sink, Process, Monitor
+from SimComponents import Source, Sink, Process
+from Postprocessing import Utilization, ArrivalRateAndThroughput, Queue
 
+# 코드 실행 시작 시각
 start_0 = time.time()
+
+# data 받아오기
 data_all = pd.read_excel('./data/spool_data_for_simulation.xlsx')
 data_all = data_all[['NO_SPOOL', '제작협력사', '도장협력사', "Plan_makingLT", "Actual_makingLT", "Predicted_makingLT",
                  "Plan_paintingLT", "Actual_paintingLT", "Predicted_paintingLT"]]
 
-# df['part_no'] = data['NO_SPOOL']
-data = data_all.rename(columns={'제작협력사': 'process1', '도장협력사': 'process2'}, inplace=False)
+data = data_all.rename(columns={'제작협력사': 'process1', '도장협력사': 'process2', 'NO_SPOOL': 'part'}, inplace=False)
 data['process1'] = data['process1'] + '_1'
 data['process2'] = data['process2'] + '_2'
 
-## IAT
-IAT = st.expon.rvs(loc=28, scale=1, size=len(data))  # 첫 번째 공정의 작업시간의 평균 = 27.9
-start_time = IAT.cumsum()
+# DATA PRE-PROCESSING
+# part 정보
+df_part = pd.DataFrame(data["part"])
 
-# Source에 넣어 줄 dataframe
+# 작업 정보
 columns = pd.MultiIndex.from_product([[0, 1, 2], ['start_time', 'process_time', 'process']])
 df = pd.DataFrame([], columns=columns)
 
 # start_time
+# IAT
+IAT = st.expon.rvs(loc=28, scale=1, size=len(data))  # 첫 번째 공정의 작업시간의 평균 = 27.9
+start_time = IAT.cumsum()
 df[(0, 'start_time')] = start_time
 df[(1, 'start_time')] = 0
 df[(2, 'start_time')] = None
@@ -38,44 +44,87 @@ df[(0, 'process')] = data['process1']
 df[(1, 'process')] = data['process2']
 df[(2, 'process')] = 'Sink'
 
-# Simulation
-RUN_TIME = 45000
+# part 정보와 process 정보의 dataframe 통합(열 기준)
+df = pd.concat([df_part, df], axis=1)
 
+# Modeling
 env = simpy.Environment()
 
-process_dict = {}
-Source = Source(env, 'Source', df, process_dict, len(df), data_type="df")
-Sink = Sink(env, 'Sink', rec_lead_time=True, rec_arrivals=True)
-
+##
+event_tracer = {"event": [], "time": [], "part": [], "process": []}
 process_list = list(data.drop_duplicates(['process1'])['process1'])
 process_list += list(data.drop_duplicates(['process2'])['process2'])
-
+process_dict = {}
 process = []
-for i in range(len(process_list)):
-    process.append(Process(env, process_list[i], 1, process_dict, 10000))
+m_dict = {}
 
+# Source, Sink modeling
+Source = Source(env, 'Source', df, process_dict, len(df), event_tracer=event_tracer, data_type="df")
+Sink = Sink(env, 'Sink', rec_lead_time=True, rec_arrivals=True)
+# Process modeling
+for i in range(len(process_list)):
+    m_dict[process_list[i]] = 1
+    process.append(Process(env, process_list[i], m_dict[process_list[i]], process_dict, event_tracer=event_tracer, qlimit=10000))
 for i in range(len(process_list)):
     process_dict[process_list[i]] = process[i]
-
 process_dict['Sink'] = Sink
 
+# Simulation
 start = time.time()
 env.run()
 finish = time.time()
 
-print("total time ", finish - start_0)
-print("simulation time : ", finish - start)
+print('#' * 80)
+print("Results of simulation")
+print('#' * 80)
 
+# 코드 실행 시간
+print("data pre-processing : ", start - start_0)
+print("total time : ", finish - start_0)
+print("simulation execution time :", finish - start)
 
-def utilization(activity):
-    temp_process = process_dict[activity]
-    total_time = (temp_process.process_finish - temp_process.process_start) * temp_process.server_num
-    total_working = temp_process.working_time
-    u = total_working / total_time
+# 총 리드타임 - 마지막 part가 Sink에 도달하는 시간
+print("Total Lead Time :", Sink.last_arrival, "\n")
 
-    return u
+# save data
+save_path = './result'
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
 
+# event tracer dataframe으로 변환
+df_event_tracer = pd.DataFrame(event_tracer)
+df_event_tracer.to_excel(save_path +'/event_supply_chain.xlsx')
+
+# DATA POST-PROCESSING
+# Event Tracer을 이용한 후처리
+print('#' * 80)
+print("Data Post-Processing")
+print('#' * 80)
+
+# 가동률
+Utilization = Utilization(df_event_tracer, process_dict, process_list)
+Utilization.utilization()
+utilization = Utilization.u_dict
 
 for process in process_list:
-    print('utilization of {} : '.format(process), utilization(process))
+    print("utilization of {} : ".format(process), utilization[process])
 
+# Arrival rate, Throughput
+ArrivalRateAndThroughput = ArrivalRateAndThroughput(df_event_tracer, process_list)
+ArrivalRateAndThroughput.arrival_rate()
+ArrivalRateAndThroughput.throughput()
+arrival_rate = ArrivalRateAndThroughput.process_arrival_rate
+throughput = ArrivalRateAndThroughput.process_throughput
+
+print('#' * 80)
+print("Arrival rate : ", arrival_rate)
+print("Throughput : ", throughput)
+
+# process 별 평균 대기시간, 총 대기시간
+Queue = Queue(df_event_tracer, process_list)
+Queue.waiting_time()
+print('#' * 80)
+for process in process_list:
+    print("average waiting time of {} : ".format(process), Queue.average_waiting_time_dict[process])
+for process in process_list:
+    print("total waiting time of {} : ".format(process), Queue.total_waiting_time_dict[process])
