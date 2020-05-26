a31fa8771a66151512af2f757d83a8502066f83f
SohyunNam <56374318+SohyunNam@users.noreply.github.com>
2020-05-26 09:07:38 +0900

    revision

--- PBS_df.py
+++ PBS_df.py
@@ -1,129 +1,116 @@
 import os
 import pandas as pd
-import numpy as np
 import scipy.stats as st
 import simpy
 import time
 
 from SimComponents import Source, Sink, Process
-from Postprocessing import Utilization, ArrivalRateAndThroughput, Queue
+from Postprocessing import Utilization, Queue
 
+# 코드 실행 시작 시각
+start_0 = time.time()
 
-if __name__ == '__main__':
+# csv 파일 pandas 객체 생성 // 000, 003, fin 중 선택 가능
+data_all = pd.read_csv('./data/PBS_assy_sequence_gen_fin.csv')
+data = data_all[["product", "plate_weld", "saw_front", "turn_over", "saw_back", "longi_weld", "unit_assy", "sub_assy"]]
 
-    start_0 = time.time()
+# process list
+process_list = ["plate_weld", "saw_front", "turn_over", "saw_back", "longi_weld", "unit_assy", "sub_assy"]
 
-    # csv 파일 pandas 객체 생성 // 000, 003, fin 중 선택 가능
-    data_all = pd.read_csv('./data/PBS_assy_sequence_gen_fin.csv')
-    data = data_all[["product", "plate_weld", "saw_front", "turn_over", "saw_back", "longi_weld", "unit_assy", "sub_assy"]]
+# DATA PRE-PROCESSING
+# part 정보
+df_part = pd.DataFrame(data["product"])
+df_part = df_part.rename(columns={"product": "part"})
 
-    # process list
-    process_list = ["plate_weld", "saw_front", "turn_over", "saw_back", "longi_weld", "unit_assy", "sub_assy"]
+# 작업 정보, 7 = 공정 수 + Sink
+columns = pd.MultiIndex.from_product([[i for i in range(len(process_list)+1)], ['start_time', 'process_time', 'process']])
+df = pd.DataFrame([], columns=columns)
 
-    # DATA PRE-PROCESSING
-    # part 정보
-    df_part = pd.DataFrame(data["product"])
-    df_part = df_part.rename(columns={"product": "part"})
+IAT = st.expon.rvs(loc=3, scale=1, size=len(data))
+start_time = IAT.cumsum()
 
-    # 작업 정보, 7 = 공정 수 + Sink
-    columns = pd.MultiIndex.from_product([[i for i in range(7)], ['start_time', 'process_time', 'process']])
-    df = pd.DataFrame([], columns=columns)
+for i in range(len(process_list) + 1):
+    if i == len(process_list):  # Sink
+        df[(i, 'start_time')] = None
+        df[(i, 'process_time')] = None
+        df[(i, 'process')] = 'Sink'
+    else:  # 공정
+        if i == 0:
+            df[(i, 'start_time')] = start_time
+        else:
+            df[(i, 'start_time')] = 0
 
-    IAT = st.expon.rvs(loc=3, scale=1, size=len(data))
-    start_time = IAT.cumsum()
+        df[(i, 'process_time')] = data[process_list[i]]
+        df[(i, 'process')] = process_list[i]
 
-    for i in range(len(process_list) + 1):
-        if i == len(process_list):  # Sink
-            df[(i, 'start_time')] = None
-            df[(i, 'process_time')] = None
-            df[(i, 'process')] = 'Sink'
-        else:  # 공정
-            if i == 0:
-                df[(i, 'start_time')] = start_time
-            else:
-                df[(i, 'start_time')] = 0
+df = pd.concat([df_part, df], axis=1)
 
-            df[(i, 'process_time')] = data[process_list[i]]
-            df[(i, 'process')] = process_list[i]
+# Modeling
+env = simpy.Environment()
 
-    df = pd.concat([df_part, df], axis=1)
+##
+event_tracer = {"event": [], "time": [], "part": [], "process": []}
+process_dict = {}
+process = []
+m_dict = {}
 
-    # Modeling
-    env = simpy.Environment()
+# Source, Sink modeling
+Source = Source(env, 'Source', df, process_dict, len(df), event_tracer=event_tracer, data_type="df")
+Sink = Sink(env, 'Sink', rec_lead_time=True, rec_arrivals=True)
 
-    ##
-    event_tracer = {"event": [], "time": [], "part": [], "process": []}
-    process_dict = {}
-    process = []
-    m_dict = {}
+# process modeling
+for i in range(len(process_list)):
+    m_dict[process_list[i]] = 1
+    process.append(Process(env, process_list[i], m_dict[process_list[i]], process_dict, event_tracer=event_tracer, qlimit=2))
+for i in range(len(process_list)):
+    process_dict[process_list[i]] = process[i]
+process_dict['Sink'] = Sink
 
-    # Source, Sink modeling
-    Source = Source(env, 'Source', df, process_dict, len(df), event_tracer=event_tracer, data_type="df")
-    Sink = Sink(env, 'Sink', rec_lead_time=True, rec_arrivals=True)
-    # process modeling
-    for i in range(len(process_list)):
-        m_dict[process_list[i]] = 1
-        process.append(Process(env, process_list[i], m_dict[process_list[i]], process_dict, event_tracer=event_tracer, qlimit=2))
-    for i in range(len(process_list)):
-        process_dict[process_list[i]] = process[i]
-    process_dict['Sink'] = Sink
+# Simulation
+start = time.time()  # 시뮬레이션 실행 시작 시각
+env.run()
+finish = time.time()  # 시뮬레이션 실행 종료 시각 
 
-    # Simulation
-    start = time.time()
-    env.run()
-    finish = time.time()
+print('#' * 80)
+print("Results of simulation")
+print('#' * 80)
 
-    print('#' * 80)
-    print("Results of simulation")
-    print('#' * 80)
+# 코드 실행 시간
+print("data pre-processing : ", start - start_0)
+print("total time : ", finish - start_0)
+print("simulation execution time :", finish - start)
 
-    # 코드 실행 시간
-    print("data pre-processing : ", start - start_0)
-    print("total time : ", finish - start_0)
-    print("simulation execution time :", finish - start)
+# 총 리드타임 - 마지막 part가 Sink에 도달하는 시간
+print("Total Lead Time :", Sink.last_arrival, "\n")
 
-    # 총 리드타임 - 마지막 part가 Sink에 도달하는 시간
-    print("Total Lead Time :", Sink.last_arrival, "\n")
+# save data
+save_path = './result'
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
 
-    # save data
-    save_path = './result'
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
+# event tracer dataframe으로 변환
+df_event_tracer = pd.DataFrame(event_tracer)
+df_event_tracer.to_excel(save_path +'/event_PBS.xlsx')
 
-    # event tracer dataframe으로 변환
-    df_event_tracer = pd.DataFrame(event_tracer)
-    df_event_tracer.to_excel(save_path +'/event_PBS.xlsx')
+# DATA POST-PROCESSING
+# Event Tracer을 이용한 후처리
+print('#' * 80)
+print("Data Post-Processing")
+print('#' * 80)
 
-    # DATA POST-PROCESSING
-    # Event Tracer을 이용한 후처리
-    print('#' * 80)
-    print("Data Post-Processing")
-    print('#' * 80)
+# 가동율
+Utilization = Utilization(df_event_tracer, process_dict, process_list)
+Utilization.utilization()
+utilization = Utilization.u_dict
 
-    # 가동율
-    Utilization = Utilization(df_event_tracer, process_dict, process_list)
-    Utilization.utilization()
-    utilization = Utilization.u_dict
+for process in process_list:
+    print("utilization of {} : ".format(process), utilization[process])
 
-    for process in process_list:
-        print("utilization of {} : ".format(process), utilization[process])
-
-    # Arrival rate, Throughput
-    ArrivalRateAndThroughput = ArrivalRateAndThroughput(df_event_tracer, process_list)
-    ArrivalRateAndThroughput.arrival_rate()
-    ArrivalRateAndThroughput.throughput()
-    arrival_rate = ArrivalRateAndThroughput.process_arrival_rate
-    throughput = ArrivalRateAndThroughput.process_throughput
-
-    print('#' * 80)
-    print("Arrival rate : ", arrival_rate)
-    print("Throughput : ", throughput)
-
-    # process 별 평균 대기시간, 총 대기시간
-    Queue = Queue(df_event_tracer, process_list)
-    Queue.waiting_time()
-    print('#' * 80)
-    for process in process_list:
-        print("average waiting time of {} : ".format(process), Queue.average_waiting_time_dict[process])
-    for process in process_list:
-        print("total waiting time of {} : ".format(process), Queue.total_waiting_time_dict[process])
+# process 별 평균 대기시간, 총 대기시간
+Queue = Queue(df_event_tracer, process_list)
+Queue.waiting_time()
+print('#' * 80)
+for process in process_list:
+    print("average waiting time of {} : ".format(process), Queue.average_waiting_time_dict[process])
+for process in process_list:
+    print("total waiting time of {} : ".format(process), Queue.total_waiting_time_dict[process])
