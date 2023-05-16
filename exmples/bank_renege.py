db93d93ccb245eb7ba0b46eedcff309bf50e4c93
jonghunwoo <j.woo@snu.ac.kr>
2023-05-16 09:53:12 +0900

    bank_renege update

--- exmples/bank_renege.py
+++ exmples/bank_renege.py
@@ -17,8 +17,8 @@ import simpy
 
 
 RANDOM_SEED = 42
-NEW_CUSTOMERS = 5  # Total number of customers
-INTERVAL_CUSTOMERS = 10.0  # Generate new customers roughly every x seconds
+NEW_CUSTOMERS = 500  # Total number of customers
+INTERVAL_CUSTOMERS = 3.0  # Generate new customers roughly every x min
 MIN_PATIENCE = 1  # Min. customer patience
 MAX_PATIENCE = 3  # Max. customer patience
 
@@ -65,4 +65,4 @@ env = simpy.Environment()
 # Start processes and run
 counter = simpy.Resource(env, capacity=1)
 env.process(source(env, NEW_CUSTOMERS, INTERVAL_CUSTOMERS, counter))
-env.run()
+env.run(200)
